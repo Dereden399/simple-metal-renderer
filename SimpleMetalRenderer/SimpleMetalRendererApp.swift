711c70e279331416ae53a64d7651f1ed231b5719
denis <denis.kuznetsov@aalto.fi>
2024-06-27 20:40:32 +0300

    Initial Commit

---
+++ SimpleMetalRenderer/SimpleMetalRendererApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMetalRendererApp.swift
+//  SimpleMetalRenderer
+//
+//  Created by Denis Kuznetsov on 27.6.2024.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMetalRendererApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
